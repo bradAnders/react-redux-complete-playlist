e48adedbb9998dd8ade8cf4be56b58a7b25757c1
Brad <anderson.bradley.thomas@gmail.com>
2020-07-11 08:11:22 -0700

    programatic redirect

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
-        <a className="brand-logo">Poke'Times</a>
+        <Link to="/" className="brand-logo">Poke'Times</Link>
         <ul className="right">
           <li><Link to="/">Home</Link></li>
           <li><NavLink to="/about">About</NavLink></li>
